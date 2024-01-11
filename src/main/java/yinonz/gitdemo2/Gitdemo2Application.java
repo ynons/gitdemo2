25120979624d23776177edac22478fc25da03337
ynons <isreall2003@gmail.com>
2024-01-11 14:57:51 +0200

    first commit

---
+++ src/main/java/yinonz/gitdemo2/Gitdemo2Application.java
@@ -0,0 +1,13 @@
+package yinonz.gitdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitdemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitdemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/yinonz/gitdemo2/Gitdemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package yinonz.gitdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitdemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
